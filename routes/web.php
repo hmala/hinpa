9b975b5bbab1253d9996dcbb117ddb853408cfbe
hmala <abc.majd.mh@gmail.com>
2024-10-05 23:15:56 +0300

    v2

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,7 @@
 <?php
-
+  
+  use App\Http\Controllers\Auth\RegisterController;
+  use App\Http\Controllers\HomeController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -16,3 +18,16 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+
+
+Auth::routes();
+Route::middleware(['2fa'])->group(function () {
+   
+    Route::get('/home', [HomeController::class, 'index'])->name('home');
+    Route::post('/2fa', function () {
+        return redirect(route('home'));
+    })->name('2fa');
+});
+  
+Route::get('/complete-registration', [RegisterController::class, 'completeRegistration'])->name('complete.registration');
